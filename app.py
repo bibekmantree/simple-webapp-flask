c145be95b19982b0824ab3362428b3f2ae2582e9
tapanunix <tapanunix@outlook.com>
2024-05-23 00:15:34 +0530

    Changed to Hari

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "OMM NAMHA Ganapati!"
+    return "OMM NAMHA Haribolo!"
 
 @app.route('/how are you')
 def hello():
-    return 'BABA Sivaya'
+    return 'Hari Hari kara pari'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
